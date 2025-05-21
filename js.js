fbab6f1de56ee5fe07f72b612b61a93830b54123
Jensen Tax <1094489@apps.nsd.org>
2025-05-21 16:13:31 +0000

    i think it is working

--- js.js
+++ js.js
@@ -1,1 +1,23 @@
- alert("baked beans");
+alert("PLEASE LET IT WORK!!!!!!!!!!!!!!!!")
+var LIST = [
+"tralalero tralala",
+"chicken jockey",
+"everything else"
+];
+
+var BTN = document.getElementById("myButton");
+
+var theAction = function(event) {
+alert("I love you");
+};
+
+
+for (var i = 0; i < 3; i++) {
+var newBTN = BTN.cloneNode(true);
+newBTN.innerHTML = LIST[i];
+document.body.append(newBTN);
+alert("CHECK MeOUT!!!!!!!! "  + LIST[i] );
+
+newBTN.addEventListener("click", theAction);
+};
+
